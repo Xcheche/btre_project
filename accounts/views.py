050850d8b5e442d37f86670a57cd30f628fcaf3f
xcheche <chekwubechukwu23@gmail.com>
2024-07-23 06:41:36 +0100

    still in progress

--- accounts/views.py
+++ accounts/views.py
@@ -3,11 +3,16 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def register(request):
-    return render(request, "accounts/register.html")
+    if request.method == "POST":
+   # register logic
+     return render(request, "accounts/register.html")
 
 
 def login(request):
-    return render(request, "accounts/login.html")
+    if request.method == "POST":
+   # login logic
+   
+     return render(request, "accounts/login.html")
 
 
 def logout(request):
